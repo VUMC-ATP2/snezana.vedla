9cda50db8576978a464c3613bbd45b8bfc66a41d
snezana.vedla <daugavietesne@inbox.lv>
2022-05-16 21:45:55 +0300

    commit all

---
+++ src/main/java/classroom/MyFirstJavaCode.java
@@ -0,0 +1,27 @@
+package classroom;
+
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+        // byte data type
+        byte floorCount = 7;
+        System.out.println("My house has " + floorCount + " floors");
+        System.out.printf("My house has %d floor",floorCount);
+
+        //%d - numbers
+        //%s - text
+        //%b - boolean
+
+        short salaryInEur = 1000;
+        System.out.println(salaryInEur);
+        System.out.println(1200);
+
+        boolean isSummer = false;
+        boolean isSpring = true;
+        System.out.println(isSpring);
+        System.out.println(isSummer);
+
+
+
+
+    }
+}
